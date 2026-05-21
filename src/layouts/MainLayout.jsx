ab9cd868b269e87bdbff55fc0e8dc2cf4b4d0f2d
Parthiv Binish <161676164+Parthiv-Binish@users.noreply.github.com>
2026-05-21 09:35:50 +0530

    Add BottomBar and adjust layout structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import { useSearchParams } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 import Sidebar from '../components/layout/Sidebar'
+import BottomBar from '../components/layout/BottomBar' // Import bottom bar asset
+import AnnouncementModal from '../components/ui/AnnouncementModal'
 
 export default function MainLayout({ children }) {
   const [, setSearchParams] = useSearchParams()
@@ -10,14 +12,22 @@ export default function MainLayout({ children }) {
   }
 
   return (
-    <div className="min-h-screen bg-valo-dark">
+    <div className="min-h-screen bg-valo-dark text-white flex flex-col">
+      <AnnouncementModal />
       <Navbar onSearch={handleSearch} />
+      
+      {/* Sidebar - Visible only on large screens */}
       <Sidebar />
-      <main className="pt-14 lg:pl-56">
-        <div className="p-4 md:p-6">
+      
+      {/* Main Content Area adjustments */}
+      <main className="pt-14 lg:pl-56 flex-1">
+        <div className="p-4 md:p-6 pb-20 lg:pb-6">
           {children}
         </div>
       </main>
+
+      {/* Bottom Bar - Mobile Navigation display overlay */}
+      <BottomBar />
     </div>
   )
 }
