be99206f7a0a0d5c0d62287518aed7cc0430d751
Parthiv Binish <161676164+Parthiv-Binish@users.noreply.github.com>
2026-06-01 15:16:40 +0530

    Refactor AllStreamersPage component structure

--- src/pages/AllStreamersPage.jsx
+++ src/pages/AllStreamersPage.jsx
@@ -31,14 +31,7 @@ export default function AllStreamersPage() {
     }
     fetchAllActiveBanners()
   }, [streamers])
-useEffect(() => {
-  if (streamers?.length > 0) {
-    const ytLive = streamers.find(s => s.platform === 'youtube' && s.isLive);
-    const ytOffline = streamers.find(s => s.platform === 'youtube' && !s.isLive);
-    console.log('YT LIVE:', JSON.stringify(ytLive, null, 2));
-    console.log('YT OFFLINE:', JSON.stringify(ytOffline, null, 2));
-  }
-}, [streamers]);
+
   const safeStreamers = useMemo(() => streamers || [], [streamers])
 
   const filtered = useMemo(() => {
