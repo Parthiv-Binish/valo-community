74da97fc136ce87fb98452530d08b94572f8c429
Parthiv Binish <161676164+Parthiv-Binish@users.noreply.github.com>
2026-09-21 01:32:36 +0530

    Refactor MainLayout to include BottomBar component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 import Sidebar from '../components/layout/Sidebar'
-import BottomBar from '../components/layout/BottomBar' // Import bottom bar asset
+import BottomBar from '../components/layout/BottomBar'
 import AnnouncementModal from '../components/ui/AnnouncementModal'
 
 export default function MainLayout({ children }) {
@@ -12,21 +12,25 @@ export default function MainLayout({ children }) {
   }
 
   return (
-    <div className="min-h-screen bg-valo-dark text-white flex flex-col">
+    <div className="min-h-screen bg-valo-dark text-white flex flex-col overflow-x-hidden">
       <AnnouncementModal />
+
       <Navbar onSearch={handleSearch} />
-      
-      {/* Sidebar - Visible only on large screens */}
+
+      {/* Desktop navigation rail */}
       <Sidebar />
-      
-      {/* Main Content Area adjustments */}
-      <main className="pt-14 lg:pl-56 flex-1">
-        <div className="p-4 md:p-6 pb-20 lg:pb-6">
+
+      {/* Main content
+          - Desktop: reserves space for the sidebar
+          - Mobile/tablet: uses full width
+          - Bottom padding keeps content above the mobile navigation */}
+      <main className="relative flex-1 pt-14 lg:pl-56">
+        <div className="mx-auto w-full max-w-[1800px] px-3 py-4 sm:px-5 sm:py-5 md:px-6 md:py-6 lg:px-8 lg:py-7 pb-24 lg:pb-8">
           {children}
         </div>
       </main>
 
-      {/* Bottom Bar - Mobile Navigation display overlay */}
+      {/* Mobile navigation */}
       <BottomBar />
     </div>
   )
